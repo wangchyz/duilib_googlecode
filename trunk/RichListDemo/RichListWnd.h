ba4b70868ea41a4b043b391b2db4a4522329243d
daviyang35@gmail.com <daviyang35@gmail.com@0faa2dcf-3321-c2f5-ee64-8fe4bd34913b>
2012-04-08 12:51:22 +0000

    增加RichListDemo

---
+++ trunk/RichListDemo/RichListWnd.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "WinImplBase.hpp"
+
+class CRichListWnd : public WindowImplBase
+{
+public:
+	CRichListWnd(void);
+	~CRichListWnd(void);
+
+	virtual void OnFinalMessage( HWND );
+	virtual tString GetSkinFolder();
+	virtual tString GetSkinFile();
+	virtual LPCTSTR GetWindowClassName( void ) const;
+	virtual void Notify( TNotifyUI &msg );
+	virtual LRESULT OnMouseWheel( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+	virtual LRESULT OnSysCommand( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+	virtual void Init();
+	virtual LRESULT OnMouseHover( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+	virtual LRESULT OnChar( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
+
+private:
+	CButtonUI* m_pCloseBtn;
+	CButtonUI* m_pMaxBtn;
+	CButtonUI* m_pRestoreBtn;
+	CButtonUI* m_pMinBtn;
+};
