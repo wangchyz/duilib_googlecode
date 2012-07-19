b959ade5067f74a31e9883371671cd7ce735aab9
daviyang35@gmail.com <daviyang35@gmail.com@0faa2dcf-3321-c2f5-ee64-8fe4bd34913b>
2012-07-19 10:13:15 +0000

    UIRichEdit 修复2个Bug by：boogunote@gmail.com 1.TxNotify处理了EN_REQUESTRESIZE之后要给机会给CRichEditUI::OnTxNotify()继续处理 2. CRicheditUI 控件focusbordercolor属性不起作用
    UIManager 修复1个bug by：euan1022@gmail.com
    1.建立多窗口程序，同时包含了webbrowser控件时，多窗口程序内的键盘消息被webbrowser截获。
    
    x64 编译.txt
    64位编译经验 by：whooya.xxl@gmail.com

--- trunk/DuiLib/UIManager.cpp
+++ trunk/DuiLib/UIManager.cpp
@@ -2191,20 +2191,36 @@ bool CPaintManagerUI::TranslateMessage(const LPMSG pMsg)
 	HWND hwndParent = ::GetParent(pMsg->hwnd);
 	UINT uStyle = GetWindowStyle(pMsg->hwnd);
 	LRESULT lRes = 0;
+
+	bool preHandled = false;
 	for (int i = 0; i < m_aPreMessages.GetSize(); i++)
 	{
 		CPaintManagerUI *pT = static_cast<CPaintManagerUI *>(m_aPreMessages[i]);
 		if (pMsg->hwnd == pT->GetPaintWindow()
 			|| (hwndParent == pT->GetPaintWindow() && ((uStyle & WS_CHILD) != 0)))
 		{
-			if (pT->PreMessageHandler(pMsg->message, pMsg->wParam, pMsg->lParam, lRes)) 
+			if (pT->PreMessageHandler(pMsg->message, pMsg->wParam, pMsg->lParam, lRes))
 				return true;
-		}
-		else if (pMsg->hwnd != pT->GetPaintWindow())
-		{
-			pT->TranslateAccelerator(pMsg);
+
+			// 消息不再传递到TranslateAcce中，若存在多个窗口，加速键消息在webbrowser中被处理后
+			// 会导致其他窗口内Edit部分按键消息（回格键、方向键）无法响应
+			preHandled = true;
 		}
 	}
+
+	if (!preHandled)
+	{
+		for (int i = 0; i < m_aPreMessages.GetSize(); i++)
+		{
+			CPaintManagerUI *pT = static_cast<CPaintManagerUI *>(m_aPreMessages[i]);
+			if (pMsg->hwnd != pT->GetPaintWindow())
+			{
+				if (pT->TranslateAccelerator(pMsg))
+					return true;
+			}
+		}
+	}
+
 	return false;
 }
 
