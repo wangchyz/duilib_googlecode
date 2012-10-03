e62885d87d598d2ac7400c5f605a7ca0779af73b
ljh_0110@163.com <ljh_0110@163.com@0faa2dcf-3321-c2f5-ee64-8fe4bd34913b>
2012-10-03 14:58:16 +0000

    模拟MFC消息映射宏参考例子

--- trunk/RichListDemo/RichListWnd.h
+++ trunk/RichListDemo/RichListWnd.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "WinImplBase.hpp"
 
 class CRichListWnd : public WindowImplBase
 {
@@ -8,8 +7,8 @@ public:
 	~CRichListWnd(void);
 
 	virtual void OnFinalMessage( HWND );
-	virtual tString GetSkinFolder();
-	virtual tString GetSkinFile();
+	virtual CDuiString GetSkinFolder();
+	virtual CDuiString GetSkinFile();
 	virtual LPCTSTR GetWindowClassName( void ) const;
 	virtual void Notify( TNotifyUI &msg );
 	virtual LRESULT OnMouseWheel( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
@@ -18,7 +17,12 @@ public:
 	virtual LRESULT OnMouseHover( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
 	virtual LRESULT OnChar( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled );
 	virtual UILIB_RESOURCETYPE GetResourceType() const;
-	virtual tString GetZIPFileName() const;
+	virtual CDuiString GetZIPFileName() const;
+
+	DUI_DECLARE_MESSAGE_MAP()
+	virtual void OnClick(TNotifyUI& msg);
+	virtual void OnSelectChanged( TNotifyUI &msg );
+	virtual void OnItemClick( TNotifyUI &msg );
 
 private:
 	CButtonUI* m_pCloseBtn;
