995fc615db0aaa90bb92f5da671e15ed19eb823a
daviyang35@gmail.com <daviyang35@gmail.com@0faa2dcf-3321-c2f5-ee64-8fe4bd34913b>
2013-03-25 03:25:42 +0000

    1.Duilib嵌入Windows公共控件

--- DuiLib/UIlib.h
+++ DuiLib/UIlib.h
@@ -43,6 +43,16 @@
 
 #define UILIB_COMDAT __declspec(selectany)
 
+#if defined _M_IX86
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
